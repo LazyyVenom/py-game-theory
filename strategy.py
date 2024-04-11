cd5414282d1bb4073bfa3881d456ec1f940b532b
Anubhav <choubey.anubhav253@gmail.com>
2024-04-11 15:53:40 +0530

    added random

--- strategy.py
+++ strategy.py
@@ -1,4 +1,5 @@
 from typing import Callable, List
+import random
 
 class Strategy:
     def __init__(
@@ -109,6 +110,46 @@ win_stay_lose_shift = Strategy(
 
 
 def pavlov_logic(prev_results,place) -> int:
+    if len(prev_results) == 0:
+        return 1
+
+    last_score = score(prev_results[-1],place)
+
+    if prev_results[-1][place] == 1:
+        if last_score == 5:
+            return 1
+        return 0
+    
+    else:
+        if last_score == 0:
+            return 1
+        else:
+            return 0
+
+
+pavlov = Strategy(
+    name = "Pavlov",
+    st_id = "pav",
+    desc = "Strategy: Start with cooperation. Keep the same action if it resulted in the highest payoff in the previous round; switch actions otherwise.\nDescription: Reinforces successful strategies while abandoning unsuccessful ones, promoting stability.",
+    strategy_type = "NICE",
+    logic = pavlov_logic
+)
+
+
+def random_logic(prev_results=[],place=0) -> int:
+    return random.randint(0,1)
+
+
+random_st = Strategy(
+    name = "Random",
+    st_id = "ran",
+    desc = "Strategy: Choose cooperation or defection randomly, with equal probability, in each round.\nDescription: Introduces unpredictability into the game, sometimes exploiting overly deterministic opponents.",
+    strategy_type = "UNKNOWN",
+    logic = random_logic
+)
+
+
+def grim_trigger_logic(prev_results,place) -> int:
     if len(prev_results) == 0:
         return 1
 
