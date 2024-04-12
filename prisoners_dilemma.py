31c6d68ce1a200361c98cc32c8804a983b5cb089
Anubhav <choubey.anubhav253@gmail.com>
2024-04-12 19:16:48 +0530

    changed title for menu page

--- prisoners_dilemma.py
+++ prisoners_dilemma.py
@@ -8,7 +8,7 @@ pygame.init()
 screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Menu")
+pygame.display.set_caption("Prisoner's Dilemma")
 
 # Colors
 WHITE = (255, 255, 255)
