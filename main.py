514384dddca3474b0c5f32dfb413e8ec9a8f9c96
Anubhav <choubey.anubhav253@gmail.com>
2024-04-18 19:29:54 +0530

    starting work on backend

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
 # Writing The Logic without implementation on GUI (PyGame)
+from strategy import strategies
+
+flag = [True for _ in range(len(strategies))]
+
+needed_strategies = []
+
+for i in range(len(strategies)):
+    if flag[i]:
+        needed_strategies.append(strategies[i])
+
+def tournament_logic(strategies,rounds):
+    pass
