a8ed3e3ef9593662672b068f53904b32a4102751
Anubhav <choubey.anubhav253@gmail.com>
2024-04-19 10:53:20 +0530

    Fixed Tester

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Writing The Logic without implementation on GUI (PyGame)
-from strategy import strategies
+from strategy import strategies, Strategy, score
+import typing
 
 flag = [True for _ in range(len(strategies))]
 
@@ -9,16 +10,49 @@ for i in range(len(strategies)):
     if flag[i]:
         needed_strategies.append(strategies[i])
 
-def tournament_logic(strategies,rounds):
-    prev_results_list = [[] for _ in range(len(strategies))]
+def tournament_logic(strategies: typing.List[Strategy],rounds) -> typing.List[int]:
+    prev_results = []
+    scores = [0] * len(strategies)
 
-    for self_index, strategy in enumerate(strategies):
-        for opp_index, opponent_strategy in enumerate(strategies):
+    for index, strategy in enumerate(strategies):
+        for opponent_strategy in strategies:
             for round in range(rounds):
                 base_string = f"ROUND-{round} --- {strategy.name} VS {opponent_strategy.name}"
 
-                
-                print(base_string)
+                self_choice = strategy.giveNextChoice(
+                    prev_results=prev_results,place=0
+                    )
+                opp_choice = opponent_strategy.giveNextChoice(
+                    prev_results=prev_results,place=1
+                    )
+
+                prev_results.append((self_choice,opp_choice))
+
+                curr_score = score((self_choice,opp_choice),0)
+
+                # print(base_string,f"\n RESULTS: {curr_score}")
+            
+                scores[index] += curr_score
+
+            prev_results = []
+    
+    return scores
 
 if __name__ == '__main__':
-    tournament_logic(needed_strategies,20)
+    rounds = 200
+    results = tournament_logic(needed_strategies,rounds)
+        
+    winner = ""
+    winner_pts = 0
+
+    for i in range(len(strategies)):
+        if results[i] > winner_pts:
+            winner_pts = results[i]
+            winner = strategies[i]
+
+        print(f"{strategies[i].name} - {results[i]}")
+    
+    print("----------------------------------------------------------")
+    print("WINNER:",winner.name)
+    print("WINNER TYPE:",winner.strategy_type)
+    print(f"POINTS: {winner_pts}/{rounds*len(strategies)*5}")
