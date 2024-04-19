c991930e89f873b9240ef9dcdd9caaa408be2ebd
Anubhav <choubey.anubhav253@gmail.com>
2024-04-19 10:26:37 +0530

    Testing

--- main.py
+++ main.py
@@ -13,6 +13,10 @@ def tournament_logic(strategies,rounds):
     prev_results_list = [[] for _ in range(len(strategies))]
 
     for self_index, strategy in enumerate(strategies):
-        for opp_index, opponent_strategy in strategies:
-            for round in rounds:
-                strategy
+        for opp_index, opponent_strategy in enumerate(strategies):
+            for round in range(rounds):
+                print(f"ROUND-{round}({strategy.name} VS {opponent_strategy.name})")
+            
+
+if __name__ == '__main__':
+    tournament_logic(needed_strategies,20)
